bee2586b9d18d3a4ee5c303270fe8f88a704347d
Alejandro Ojeda <63747287+xeladejo@users.noreply.github.com>
2020-07-29 13:49:05 +0200

    Initial commit

---
+++ pkguser/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+export PKGDEST=$(pwd)"/${TRAVIS_TAG}"
+
+if ! grep -qs "PACKAGER" ".makepkg.conf"; then
+    export PACKAGER="${TRAVIS_REPO_SLUG/\// } <${TRAVIS_BUILD_ID}@travis.build.id>"
+fi
+
+declare -r pkgkey="DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A"
+declare -r pkgname="aurutils"
+
+if ! test -z "${pkgkey}"; then
+    gpg --recv-keys --keyserver "hkp://ipv4.pool.sks-keyservers.net" "${pkgkey}"
+fi
+
+mkdir "${TRAVIS_TAG}"
+
+git clone "https://aur.archlinux.org/${pkgname}.git"
+
+cd "${pkgname}"
+makepkg -Lcrs --noconfirm
+cd ".."
+
+cd "${TRAVIS_TAG}"
+repo-add "${TRAVIS_REPO_SLUG#*/}.db.tar.gz" *".pkg.tar.zst"
+cd ".."
+
+{ set +ex; } 2>/dev/null
